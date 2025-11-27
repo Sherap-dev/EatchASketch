63225939951917c1a00bcaf521b8a456bae25fe4
sherap <3dsherap@gmail.com>
2025-11-27 14:48:54 +0530

    add resize grid pixel logic, make the default 16*16 grid, add reset logic

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 const resizeBtn = document.querySelector("#resize");
 const resetBtn = document.querySelector("#reset");
 const randomBtn = document.querySelector("#randomColor");
-const defalutBtn = document.querySelector("#default");
+const gridContainer = document.querySelector("#grid-container")
+const grid = document.querySelector("#grid")
+
+const initialSize = 16;
+let size = undefined;
+const gridWidth = grid.clientWidth;
+const gridHeight = grid.clientHeight;
+
+window.addEventListener("load", () =>{
+    makePixels(initialSize)
+})
+
+resizeBtn.addEventListener("click",() => {
+    const sizeInput = Math.floor(prompt("please choose a number to resize the sketch pad"))
+    size = sizeInput
+    if (isNaN(size) || size <= 0){
+        alert("please add a valid number")
+    }
+   makePixels(size)
+
+})
+
+resetBtn.addEventListener("click", () => {
+    makePixels(initialSize)
+})
+
+function makePixels(size){
+    gridContainer.innerHTML = "";
+
+        for(let i = 0; i < size * size; i++){
+            const pixel = document.createElement("div")
+            const pixelSize = gridWidth / size;
+            pixel.style.width = `${pixelSize}px`
+            pixel.style.height = `${pixelSize}px`
+            pixel.classList.add("pixel")
+            gridContainer.appendChild(pixel)
+        }
+    }
+
