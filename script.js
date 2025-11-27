fa65756d64b2204a1d2f564e4eefd6e770f0f247
sherap <3dsherap@gmail.com>
2025-11-27 13:00:24 +0530

    write html and css, start js, add variables for buttons.

---
+++ script.js
@@ -0,0 +1,4 @@
+const resizeBtn = document.querySelector("#resize");
+const resetBtn = document.querySelector("#reset");
+const randomBtn = document.querySelector("#randomColor");
+const defalutBtn = document.querySelector("#default");
